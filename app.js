5c5f70443c69f2e4c55da0eb11890832da97d7f7
nienni <jiabonm@gmail.com>
2019-09-21 12:04:33 +0800

    Add new page and create feature, install body-parser

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const mongoose = require('mongoose')
 //load express handlebars
 const exphbs = require('express-handlebars')
 
+//load body-parser
+const bodyParser = require('body-parser')
+
 //set handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+//set body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //set mongoose and useNewURLParser以及不知為何出現叫我裝 { useUnifiedTopology: true }的warning
 mongoose.connect('mongodb://localhost/record', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -46,12 +52,23 @@ app.get('/records', (req, res) => {
 
 //新增介面
 app.get('/records/new', (req, res) => {
-  res.send('新增介面')
+  return res.render('new')
 })
 
 //新增
 app.post('/records', (req, res) => {
-  res.send('新增')
+  const record = new Record({
+    name: req.body.name,
+    date: req.body.date,
+    category: req.body.category,
+    amount: req.body.amount
+  })
+  // 存入資料庫
+  record.save(err => {
+    if (err) return console.error(err)
+    return res.redirect('/')
+  })
+
 })
 
 //編輯介面
